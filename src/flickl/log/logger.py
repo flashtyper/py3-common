f33576747edb61259f9578a0312719063560a0d7
Lukas <you@example.com>
2026-05-16 11:51:30 +0200

    feat: Logging methods are now supporting *args

--- src/flickl/log/logger.py
+++ src/flickl/log/logger.py
@@ -12,29 +12,37 @@ class LoggingLevels(Enum):
     NOTSET = logging.NOTSET
 
 
-class FlicklLogger:
+class Logger:
     def __init__(self, name: str):
         self.log = logging.getLogger(name)
         logging.basicConfig(encoding='utf-8', level=logging.INFO, format='[%(levelname)s %(asctime)s - %(name)s %(funcName)s %(lineno)s] %(message)s', datefmt='[%d.%m.%Y %H:%M:%S]')
+
+    def __concat_msgs(msgs: Union[str, list], delimiter=',') -> str:
+        if isinstance(msgs, str):
+            return str
+        elif isinstance(msgs, list):
+            return "{delimiter} ".join([str(msg) for msg in msgs])
+        else:
+            raise ValueError(f"Datatype {type(msgs)} not allowed here: Only str or list!")
 
     def change_level(self, level: LoggingLevels):
         self.log.setLevel(level.value)
         self.log.log(level.value, 'Changed to level {}'.format(level.name))
 
-    def info(self, message: str):
-        self.log.info(message)
+    def info(self, *messages):
+        self.log.info(messages)
 
-    def warning(self, message: str):
-        self.log.warning(message)
+    def warning(self, *messages: str):
+        self.log.warning(messages)
 
-    def error(self, message: str):
-        self.log.error(message)
+    def error(self, *messages: str):
+        self.log.error(messages)
 
-    def critical(self, message: str):
-        self.log.critical(message)
+    def critical(self, *messages: str):
+        self.log.critical(messages)
 
-    def fatal(self, message: str):
-        self.log.fatal(message)
+    def fatal(self, *messages: str):
+        self.log.fatal(messages)
 
-    def debug(self, message: str):
-        self.log.debug(message)
+    def debug(self, *messages: str):
+        self.log.debug(messages)
