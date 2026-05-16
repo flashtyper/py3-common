6fcdad95c17c6fab66d80f8eb6599bcbb97867bb
Lukas <you@example.com>
2026-05-16 11:51:55 +0200

    doc: Added doc for methods

--- src/flickl/monitoring/monitoring.py
+++ src/flickl/monitoring/monitoring.py
@@ -9,14 +9,15 @@ class Monitoring:
         self.output = []
 
     def exit_plugin(self):
+        # Prints the outputs, perfata and exits accordingly its state
         if self.perf_data:
-
             print(self.states[str(self.state)] + ' - ' + '\n'.join(self.output) + ' | ' + ' '.join(self.perf_data))
         else:
             print(self.states[str(self.state)] + ' - ' + '\n'.join(self.output))
         exit(self.state)
 
     def escalate_to(self, new_state: Union[str, int]) -> bool:
+        # Escalates internal state if new_state > internal state
         if isinstance(new_state, str):
             new_state = self.states[new_state]
         if new_state == self.state:
@@ -27,9 +28,11 @@ class Monitoring:
         return False
 
     def add_perfdata(self, label: str, value: Union[int, float], uom: str = '') -> None:
+        # Adds a perfdata to the plugin output
         self.perf_data.append(f'{label}={value}{uom}')
 
     def check_threshold(self, value: Union[int, float], low_warn, high_warn, low_crit, high_crit) -> bool:
+        # Checks if a given value lays between thresholds and escalates the internal state
         if value <= low_crit or value >= high_crit:
             self.escalate_to(2)
             return True
@@ -39,6 +42,7 @@ class Monitoring:
         return False
 
     def build_message(self, label, message):
+        # Adds a label with message to the plugin output
         self.output.append(f'{label}: {message}')
 
 
